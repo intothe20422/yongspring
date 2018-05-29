a9e003fd2be06de582e6a2daeeaabca9af82893b
201-08 <201-08@201-08-PC>
2018-05-29 16:29:43 +0900

    20180529spring

---
+++ ex1/src/main/java/org/study/spring/persistence/MemberDAO.java
@@ -0,0 +1,11 @@
+package org.study.spring.persistence;
+
+import org.study.spring.model.MemberVO;
+
+public interface MemberDAO {
+
+	public String getTime();
+	
+	public void insertMember(MemberVO vo);
+	
+}
